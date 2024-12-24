dd96afb8d294b83f64e1cf1632cf6f0ebba23ed0
Kamalpreet Kaur <38219887+kamal-kaur04@users.noreply.github.com>
2024-12-24 15:57:09 +0530

    Revert "Revert version 1.1.5"

--- src/main/java/com/browserstack/local/LocalBinary.java
+++ src/main/java/com/browserstack/local/LocalBinary.java
@@ -2,11 +2,15 @@ package com.browserstack.local;
 
 import org.apache.commons.io.FileUtils;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.File;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.regex.Pattern;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.ZipException;
 
 class LocalBinary {
 
@@ -58,7 +62,8 @@ class LocalBinary {
             throw new LocalException("Failed to detect OS type");
         }
 
-        httpPath = BIN_URL + binFileName;
+        String sourceURL = BIN_URL;
+        httpPath = sourceURL + binFileName;
     }
 
     private boolean isAlpine() {
@@ -174,7 +179,7 @@ class LocalBinary {
             URL url = new URL(httpPath);
 
             File f = new File(source);
-            FileUtils.copyURLToFile(url, f);
+            newCopyToFile(url, f);
 
             changePermissions(binaryPath);
         } catch (Exception e) {
@@ -191,5 +196,27 @@ class LocalBinary {
 
     public String getBinaryPath() {
         return binaryPath;
+    }
+
+    private static void newCopyToFile(URL url, File f) throws IOException {
+        URLConnection conn = url.openConnection();
+        conn.setRequestProperty("User-Agent", "browserstack-local-java/" + Local.getPackageVersion());
+        conn.setRequestProperty("Accept-Encoding", "gzip, *");
+        String contentEncoding = conn.getContentEncoding();
+
+        if (contentEncoding == null || !contentEncoding.toLowerCase().contains("gzip")) {
+            FileUtils.copyToFile(conn.getInputStream(), f);
+            return;
+        }
+
+        try (InputStream stream = new GZIPInputStream(conn.getInputStream())) {
+            if (System.getenv().containsKey("BROWSERSTACK_LOCAL_DEBUG_GZIP")) {
+                System.out.println("using gzip in " + conn.getRequestProperty("User-Agent"));
+            }
+
+            FileUtils.copyToFile(stream, f);
+        } catch (ZipException e) {
+            FileUtils.copyURLToFile(url, f);
+        }
     }
 }
